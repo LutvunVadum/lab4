4c7bb70f7a755aeaded34f4684920cda36da0ccb
LutvunVadum <litvinvadik313@gmail.com>
2024-09-23 22:06:13 +0300

    lab4.2.py

--- lab4.2.py
+++ lab4.2.py
@@ -2,6 +2,6 @@ import math
 x = float(input("Введіть значення х:"))
 y = float(input("Введіть значення y:"))
 
-f = math.pow((pow(x,3) + pow(y,3)), 1/4) + math.sin(3*x + 1) + math.log10(math.fabs(x)) - pow(math.e,(y-x))
+f = math.pow((math.pow(x,3) + math.pow(y,3)), 1/4) + math.sin(3*x + 1) + math.log10(math.fabs(x)) - math.pow(math.e,(y-x))
 
 print(f)
